83d920762badbbf25422f7b171f54d0412450a66
NGUYEN Tran Trung <trung.nguyentran@olbius.com>
2013-06-15 20:03:13 +0700

    small change

--- wordpress/pluggable.php
+++ wordpress/pluggable.php
@@ -317,7 +317,7 @@ function wp_mail( $to, $subject, $message, $headers = '', $attachments = array()
 	// From email and name
 	// If we don't have a name from the input headers
 	if ( !isset( $from_name ) )
-		$from_name = 'no-reply';
+		$from_name = 'noreply';
 
 	/* If we don't have an email from the input headers default to wordpress@$sitename
 	 * Some hosts will block outgoing mail from this address if it doesn't exist but
@@ -333,7 +333,7 @@ function wp_mail( $to, $subject, $message, $headers = '', $attachments = array()
 			$sitename = substr( $sitename, 4 );
 		}
 
-		$from_email = 'no-reply@' . $sitename;
+		$from_email = 'noreply@' . $sitename;
 	}
 
 	// Plugin authors can override the potentially troublesome default
